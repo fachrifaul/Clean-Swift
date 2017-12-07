edfa7bf0c4a4ebb6e8715ed83d22bbfe96a7912e
Fachri Febrian <fachripaul@gmail.com>
2017-12-08 00:39:00 +0700

    Delete space

--- Clean Swift/Unit Tests.xctemplate/___FILEBASENAME___PresenterTests.swift
+++ Clean Swift/Unit Tests.xctemplate/___FILEBASENAME___PresenterTests.swift
@@ -39,8 +39,7 @@ class ___VARIABLE_sceneName___PresenterTests: XCTestCase {
   class ___VARIABLE_sceneName___DisplayLogicSpy: ___VARIABLE_sceneName___DisplayLogic {
     var displaySomethingCalled = false
     
-    func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel)
-    {
+    func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel) {
       displaySomethingCalled = true
     }
   }
